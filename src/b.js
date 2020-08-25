b8a6b9fe86ea8ad970ad3ff7e7d680aa0473bd93
Leenawat <leenawat.pa@gmail.com>
2020-08-25 11:00:01 +0700

    duplicate method

--- src/b.js
+++ src/b.js
@@ -2,6 +2,11 @@ function helloWorld () {
   console.log("Hello World")
 }
 
+function helloWorld2 () {
+  console.log("Hello World")
+}
+
 export default {
-  helloWorld
+  helloWorld,
+  helloWorld2
 }
