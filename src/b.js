1e03b699a64ec8cd201792c4bf671b364724028c
Leenawat <leenawat.pa@gmail.com>
2020-08-25 11:02:22 +0700

    duplicate method

--- src/b.js
+++ src/b.js
@@ -1,8 +1,31 @@
 function helloWorld () {
   console.log("Hello World")
+  console.log("Hello World")
+  console.log("Hello World")
+  console.log("Hello World")
+  console.log("Hello World")
+  console.log("Hello World")
+  console.log("Hello World")
+  console.log("Hello World")
+  console.log("Hello World")
+  console.log("Hello World")
+  console.log("Hello World")
+  console.log("Hello World")
 }
 
 function helloWorld2 () {
+  console.log("Hello World")
+  console.log("Hello World")
+  console.log("Hello World")
+  console.log("Hello World")
+  console.log("Hello World")
+  console.log("Hello World")
+  console.log("Hello World")
+  console.log("Hello World")
+  console.log("Hello World")
+  console.log("Hello World")
+  console.log("Hello World")
+  console.log("Hello World")
   console.log("Hello World")
 }
 
